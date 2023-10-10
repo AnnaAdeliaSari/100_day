5c455ee93053485d6da30b489b953d5d9a110617
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-10 20:46:56 +0800

    day2
    contoh penggunaan variabel dengan menggabungkan string nama depan dan belakang

---
+++ variabel.java
@@ -0,0 +1,12 @@
+
+package day2;
+
+public class variabel {
+    public static void main(String[] args) {
+     String namadepan = " Anna ";
+     String namabelakang ="Adelia Sari";
+     String fullname = namadepan + namabelakang;
+     
+        System.out.println(fullname);
+    }
+}
